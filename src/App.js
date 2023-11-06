76019e8009e0308d2526523c0e2e1976bca3f594
jpcc0 <conleyjoshuakm@gmail.com>
2023-11-06 17:42:49 -0500

    add backend logic/fullstack needs + tw css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubscribe = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('/subscribe', { email });
+      setMessage(response.data.message);
+      setEmail('');
+    } catch (error) {
+      setMessage('There was an error subscribing');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="flex flex-col h-screen justify-center items-center bg-gray-100">
+      <img src="/logo.png" alt="Logo" className="mb-8" />
+      <form onSubmit={handleSubscribe} className="flex flex-col items-center">
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Enter your email"
+          className="p-2 border border-gray-300 rounded mb-4"
+          required
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 text-white p-2 rounded"
         >
-          Learn React
-        </a>
-      </header>
+          Subscribe
+        </button>
+      </form>
+      {message && <p className="text-green-500 mt-4">{message}</p>}
+      <footer className="absolute bottom-0 w-full text-center p-4">
+        <a href="/privacy-policy" className="text-gray-600 text-sm">Privacy Policy</a>
+      </footer>
     </div>
   );
 }
